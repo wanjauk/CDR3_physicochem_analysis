09c02647359dbe1f9b0759321683e3e1f4e92ff2
Kennedy Mwangi <wanjaukm@gmail.com>
2021-06-26 14:51:26 +0300

    add code to analyze rnaseq data

--- prepare_RNA-seq_input_data.R
+++ prepare_RNA-seq_input_data.R
@@ -1,7 +1,7 @@
 library(openxlsx)
 library(tidyverse)
 
-rnaseq_input <- read.xlsx("RNAseq based CDR3s for aromaticity and UH.xlsx")
+rnaseq_input <- read.xlsx("RNA-seq_data/RNAseq based CDR3s for aromaticity and UH with alpha beta TCR indicated.xlsx")
 
 cancers <- c("LUSC", "KIRC", "CESC",
              "LUAD", "PAAD", "COAD",
@@ -15,12 +15,15 @@ for (cancer in cancers){
   # rename the columns
   cancer_cdr3_properties <- cancer_cdr3_properties %>% rename_at("SampleTypeLetterCode", ~"Sample.Type")
   cancer_cdr3_properties <- cancer_cdr3_properties %>% rename_at("ParticipantBarcode", ~"Filename")
+  cancer_cdr3_properties <- cancer_cdr3_properties %>% mutate(Sample.Type = ifelse(Sample.Type == "TP", "Primary Tumor", Sample.Type),
+                                                              Sample.Type = ifelse(Sample.Type == "TM", "Metastatic", Sample.Type))
+  cancer_cdr3_properties <- cancer_cdr3_properties %>% mutate(Receptor = ifelse(chain == "alpha", "TRA", "TRB"))
   
   #exclude NAs
   cancer_cdr3_properties <- cancer_cdr3_properties[complete.cases(cancer_cdr3_properties),]
   
   # write to excel file
-  write.xlsx(cancer_cdr3_properties, file = paste0("VDJ_Recoveries Boris ", cancer,".xlsx"), sheetName = "physicochem")
+  write.xlsx(cancer_cdr3_properties, file = paste0("RNA-seq_data/VDJ_Recoveries Boris ", cancer,".xlsx"), sheetName = "physicochem")
   
   
 }
--- run_all.R
+++ run_all.R
@@ -1,13 +1,13 @@
 
-# # cancers under analysis
-# cancers <- c("LUSC", "KIRC", "CESC",
-#             "LUAD", "PAAD", "COAD",
-#             "BLCA", "BRCA", "PRAD",
-#             "SKCM")
+# cancers under analysis
+cancers <- c("LUSC", "KIRC", "CESC",
+            "LUAD", "PAAD", "COAD",
+            "BLCA", "BRCA", "PRAD",
+            "SKCM")
 
-cancers <- c("BRCA", "LUSC")
 # input_data_dirs <- c("Exome_data/", "RNA-seq_data/")
-input_data_dirs <- c("Exome_data/")
+input_data_dirs <- c("RNA-seq_data/")
+
 
 for (input_data_dir in input_data_dirs){
   
@@ -19,8 +19,40 @@ for (input_data_dir in input_data_dirs){
   receptors <- c("TRA", "TRB")
   aromatic_aa <- c("F", "W", "Y")
   
+  # 1. Tumor
+  sample <- "tumor"
+  sample_type <- c("Metastatic","Primary Tumor")
   
-  # 1. Blood
+  ###################
+  # Aromaticity
+  ###################
+  physicochemical_property <- "aromaticity"
+  analysis_strategy <- "aromaticity_average"
+  source("run_CDR3_physicochem_analysis.R")
+  
+  analysis_category <- "average"
+  source("run_survival_analysis.R")
+  
+  analysis_category <- "jacki_factor"
+  source("run_survival_analysis.R")
+  
+  
+  
+  ##########################
+  # Uversky Hydropathy
+  ##########################
+  physicochemical_property <- "uversky_hydropathy"
+  analysis_strategy <- "uversky_hydropathy_average"
+  source("run_CDR3_physicochem_analysis.R")
+  
+  analysis_category <- "average"
+  source("run_survival_analysis.R")
+  
+  analysis_category <- "jacki_factor"
+  source("run_survival_analysis.R")
+  
+  
+  # 2. Blood
   
   sample <- "blood"
   sample_type <- "Blood Derived Normal"
@@ -54,37 +86,6 @@ for (input_data_dir in input_data_dirs){
       
   
   
-  # 2. Tumor
-  sample <- "tumor"
-  sample_type <- c("Metastatic","Primary Tumor")
-  
-      ###################
-      # Aromaticity
-      ###################
-      physicochemical_property <- "aromaticity"
-      analysis_strategy <- "aromaticity_average"
-      source("run_CDR3_physicochem_analysis.R")
-      
-      analysis_category <- "average"
-      source("run_survival_analysis.R")
-      
-      analysis_category <- "jacki_factor"
-      source("run_survival_analysis.R")
-      
-      
-      
-      ##########################
-      # Uversky Hydropathy
-      ##########################
-      physicochemical_property <- "uversky_hydropathy"
-      analysis_strategy <- "uversky_hydropathy_average"
-      source("run_CDR3_physicochem_analysis.R")
-      
-      analysis_category <- "average"
-      source("run_survival_analysis.R")
-      
-      analysis_category <- "jacki_factor"
-      source("run_survival_analysis.R")
   
 }
 
